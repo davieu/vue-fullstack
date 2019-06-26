6a115c714bd265a75891d5c7a87378fa56357fc7
davieu <davisumana@gmail.com>
2019-06-26 01:54:05 -0400

    added static folder and prep single page application

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
+//for anything that is /api then we want to add a proxy for localhost:5000
 module.exports = {
+  outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     proxy: {
       '/api': {
